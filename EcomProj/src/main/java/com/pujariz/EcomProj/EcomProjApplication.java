d24fb095993652bf0e339d9fd9b444d6764b68a7
AVINASH PANDEY <avinashpandey@AVINASHs-MacBook-Air.local>
2024-12-16 18:25:10 +0530

    Ecom-Project

---
+++ EcomProj/src/main/java/com/pujariz/EcomProj/EcomProjApplication.java
@@ -0,0 +1,18 @@
+package com.pujariz.EcomProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLOutput;
+
+@SpringBootApplication
+public class EcomProjApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomProjApplication.class, args);
+
+//		System.out.println("Hello!!");
+	}
+
+}
